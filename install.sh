e865af423ed0b96cc81c64e8bad77dcfca947405
Mikhail Komarov <nemo1369@gmail.com>
2019-07-05 06:01:05 +0300

    Minor updates.

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-sudo apt-get -qq install build-essential -y
-sudo apt-get -qq install yasm -y
-sudo apt-get -qq install autoconf autotools-dev libtool texinfo -y
-sudo apt-get -qq install cmake git m4 autogen automake -y
+sudo apt-get install build-essential -y > /dev/null 2>&1
+sudo apt-get install yasm -y > /dev/null 2>&1
+sudo apt-get install autoconf autotools-dev libtool texinfo -y > /dev/null 2>&1
+sudo apt-get install cmake git m4 autogen automake -y > /dev/null 2>&1
 
 #sudo rm -rf /usr/lib/gcc/x86_64-linux-gnu/8/../../../x86_64-linux-gnu/libgmpxx.a
 #sudo rm -rf /usr/lib/gcc/x86_64-linux-gnu/8/../../../x86_64-linux-gnu/libgmp.a
 
-sudo apt-get -qq install libmpfr-dev libflint-dev libgmp3-dev -y
+sudo apt-get install libmpfr-dev libflint-dev libgmp3-dev -y > /dev/null 2>&1
 mkdir build
 cd build
 
@@ -18,6 +18,7 @@ cd build
 #sudo make -j$(nproc) uninstall
 #cd ../
 
+#git clone git://github.com/wbhart/mpir.git > /dev/null 2>&1
 git clone -b 1-skylake-non-avx https://github.com/nemo1369/mpir.git > /dev/null 2>&1
 cd mpir
 #wget http://mpir.org/mpir-3.0.0.tar.bz2
